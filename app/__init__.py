dc1552e57160f962465617e7cd0fb7beacf4acee
Ondrej Vacek <ondrej.vacek2@tul.cz>
2021-06-11 20:48:40 +0200

    Time view adjusted to user

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,22 @@ import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
