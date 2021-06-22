6c10b0be624a75a09ace20f83d6940186ffea563
Ondrej Vacek <ondrej.vacek2@tul.cz>
2021-06-22 14:53:13 +0200

    Search feature implemented

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from elasticsearch import Elasticsearch
 
 
 db = SQLAlchemy()
@@ -49,6 +50,9 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']], timeout=60) \
+        if app.config['ELASTICSEARCH_URL'] else None
 
     if not app.debug and not app.testing:
         # Email logger
